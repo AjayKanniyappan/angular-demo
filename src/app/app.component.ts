c15da0121599baffddb89389b74dc502d3fec30f
ajaykanniyappan <ajaykanniyappan@gmail.com>
2023-08-08 17:18:27 +0530

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-demo';
+  isButton = true;
+  dateList: string[] = [];
+  addDate: any;
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.isButton = true;
+    const data = localStorage.getItem('dateList');
+    this.dateList = JSON.parse(data as string) || [];
+  }
+
+  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    const getDate = new Date(event.value as unknown as string);
+    const formattedDate = formatDate(getDate, 'MM-dd-yyyy', 'en-US');
+    if (!this.dateList.includes(formattedDate)) {
+      this.isButton = false;
+      this.addDate = formattedDate;
+      this.dateList.push(formattedDate);
+    }
+  }
+
+  preventEditAndDelete(event: KeyboardEvent) {
+    event.preventDefault();
+  }
+
+  saveDate() {
+    const list = localStorage.getItem('dateList');
+    const values = JSON.parse(list as string) || [];
+    const existingIndex = values.findIndex(
+      (date: any) => date === this.addDate
+    );
+
+    if (existingIndex === -1) {
+      values.push(this.addDate);
+      localStorage.setItem('dateList', JSON.stringify(values));
+      this.isButton = true;
+      this.snackBar.open('Date added Successfully', 'Ok');
+    } else {
+      this.snackBar.open('Entered date is already exist', 'Ok');
+      this.isButton = true;
+    }
+  }
+
+  myFilter = (d: Date | null): boolean => {
+    if (!d) {
+      return false; // No date provided
+    }
+
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const selectedDate = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+    // Allow only today's date and future dates.
+    return selectedDate >= today;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
